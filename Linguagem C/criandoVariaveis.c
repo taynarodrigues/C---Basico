65f7cd3198de6c2413a8ddf8c591f4c16638f3e0
Tayna Rodrigues <taynajesus@gmail.com>
2020-06-14 17:05:57 -0300

    continuação, utilizando variáveis

--- Linguagem C/criandoVariaveis.c
+++ Linguagem C/criandoVariaveis.c
@@ -5,11 +5,11 @@ int main()
 {
 	setlocale(LC_ALL, "");
 	
-	int minhaIdade;
-	
-	minhaIdade = 23;
+	int minhaIdade = 23;
+	int maeIdade = 48; int paiIdade = 49; 
 	
 	printf("Minha idade é = %i.\n", minhaIdade);
+    printf("Minha idade é = %i.\nPai idade = %i.\nMãe idade = %i.", minhaIdade, paiIdade, maeIdade);
 	
 	return 0;
 }
